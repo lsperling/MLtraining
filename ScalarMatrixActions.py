f8a9c291b20a6a4dbb9604a6980b50652bd2f06f
Liran Sperling <lsperling@gmail.com>
2020-04-30 18:35:12 +0300

    added more advances matrix actions - matrix multiplication, transpose and inverse

--- ScalarMatrixActions.py
+++ ScalarMatrixActions.py
@@ -25,6 +25,23 @@ div_As = A / s
 print(f"Division:\n{div_As}")
 
 # What happens if we have a Matrix + scalar?
+# it adds the scalar to each element in the matrix
 add_As = A + s
 print(f"Matrix & scalar addition:\n{add_As}")
-# it adds the scalar to each element in the matrix
+
+A = np.array([[1, 1], [1, 2]])
+B = np.array([[1, 3], [1, 2]])
+
+# Multiplying matrices:
+mul_AB = np.dot(B,A)
+print(f"Matrix multiplication:\n{mul_AB}")
+
+# Transposing a matrix: (does not change the caller)
+C = np.array([[1, 2, 3], [4, 5, 6]])
+C = C.T
+print(f"Matrix C transposed is:\n{C}")
+
+# Inverting  a matrix:
+D = np.array([[3, 4], [2, 16]])
+D = np.linalg.inv(D)
+print(f"Matrix D^-1 is:\n{D}")
